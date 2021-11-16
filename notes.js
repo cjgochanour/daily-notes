2bd7a625687796f7c27ac0ef82fb6e408d546888
cjgochanour <cjgochanour@gmail.com>
2021-11-16 11:59:04 -0600

    chapter 4 complete

---
+++ notes.js
@@ -0,0 +1,17 @@
+const notes = [
+    {
+        id: 1,
+        subject: "CSS",
+        date: "Nov 11",
+        feeling: "breezy",
+        timeSpent: 180,
+    },
+    {
+        id: 2,
+        subject: "Git",
+        date: "Nov 13",
+        feeling: "scared",
+        timeSpent: 1200,
+    },
+]
+console.log(notes)
