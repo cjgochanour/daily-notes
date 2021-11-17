b92b678c48e4b175f853f51d1574fe5fecddfe28
cjgochanour <cjgochanour@gmail.com>
2021-11-17 13:36:32 -0600

    added string interpolation, completed ch7

--- notes.js
+++ notes.js
@@ -20,11 +20,19 @@ const noteAboutToday = {
     id: 3,
     subject: "JavaScript",
     date: "Nov 17",
-    feeling: "logic isn't real, it can't hurt me",
+    feeling: "that logic isn't real, it can't hurt me",
     timeSpent: 120,
 }
 
 notes.push(noteAboutToday)
 
-
-console.log(notes)
+for (const note of notes) {
+    console.log(`
+    Note ${note.id}
+    ${note.date}
+    I learned about ${note.subject}
+    I spent ${note.timeSpent} working on it.
+    I felt ${note.feeling}
+    -------------
+    `)
+}
